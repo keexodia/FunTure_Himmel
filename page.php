b557afa235b1ed834b96ebe9de8ef8ea35841c5f
keexodia <71335482+keexodia@users.noreply.github.com>
2021-09-25 02:53:54 +0900

    page.php

--- page.php
+++ page.php
@@ -1,3 +1,71 @@
 <?php get_header(); ?>
 
+    <section class="main-content l-single-content p-single">
+        <div class="l-page__inner p-single__inner">
+            <p class="p-single__subtitle">news</p>
+            <h2 class="c-title c-title-big p-single__title">
+            lecture
+            <span>講演</span>
+            </h2>
+            <div class="p-page__wrapper">
+            <div class="p-page__contents" id="page-content">
+                <p class="p-page__image"><img src="./assets/img/ear.png"></p>
+
+                <div class="p-page__container">
+                <h2>各種委員、審議委員など</h2>
+                <ul>
+                    <li>日本建築学会論文審査委員長 -2017</li>
+                    <li>日本建築学会論文審査委員長 -2017</li>
+                    <li>日本建築学会論文審査委員長 -2016</li>
+                    <li>日本建築学会論文審査委員長 -2016</li>
+                    <li>日本建築学会論文審査委員長 -2016</li>
+                    <li>日本建築学会論文審査委員長 -2016</li>
+                    <li>日本建築学会論文審査委員長 -2016</li>
+                    <li>日本建築学会論文審査委員長 -2016</li>
+                    <li>日本建築学会論文審査委員長 -2016</li>
+                    <li>日本建築学会論文審査委員長 -2016</li>
+                    <li>日本建築学会論文審査委員長 -2017</li>
+                    <li>日本建築学会論文審査委員長 -2017</li>
+                    <li>日本建築学会論文審査委員長 -2016</li>
+                    <li>日本建築学会論文審査委員長 -2017</li>
+                    <li>日本建築学会論文審査委員長 -2017</li>
+                    <li>日本建築学会論文審査委員長 -2016</li>
+                    <li>日本建築学会論文審査委員長 -2016</li>
+                    <li>日本建築学会論文審査委員長 -2016</li>
+                    <li>日本建築学会論文審査委員長 -2016</li>
+                    <li>日本建築学会論文審査委員長 -2016</li>
+                    <li>日本建築学会論文審査委員長 -2016</li>
+                    <li>日本建築学会論文審査委員長 -2016</li>
+                    <li>日本建築学会論文審査委員長 -2016</li>
+                    <li>日本建築学会論文審査委員長 -2017</li>
+                    <li>日本建築学会論文審査委員長 -2017</li>
+                    <li>日本建築学会論文審査委員長 -2016</li>
+                    <li>日本建築学会論文審査委員長 -2016</li>
+                    <li>日本建築学会論文審査委員長 -2016</li>
+                    <li>日本建築学会論文審査委員長 -2016</li>
+                    <li>日本建築学会論文審査委員長 -2016</li>
+                    <li>日本建築学会論文審査委員長 -2016</li>
+                    <li>日本建築学会論文審査委員長 -2017</li>
+                    <li>日本建築学会論文審査委員長 -2017</li>
+                    <li>日本建築学会論文審査委員長 -2016</li>
+                    <li>日本建築学会論文審査委員長 -2016</li>
+                    <li>日本建築学会論文審査委員長 -2016</li>
+                    <li>日本建築学会論文審査委員長 -2016</li>
+                    <li>日本建築学会論文審査委員長 -2016</li>
+                    <li>日本建築学会論文審査委員長 -2016</li>
+                    <li>日本建築学会論文審査委員長 -2017</li>
+                    <li>日本建築学会論文審査委員長 -2017</li>
+                </ul>
+                </div>
+            </div>
+        </section>
+
+        <!-- breadcrumb -->
+        <div class="l-breadcrumb-area">
+        <ul class="p-breadcrumb">
+            <li><a href="">HOME</a></li>
+            <li><a href="">ページ名</a></li>
+        </ul>
+        </div>
+        <!-- /.breadcrumb -->
 <?php get_footer();
