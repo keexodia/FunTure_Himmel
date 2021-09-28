5509413a7fbc6ff284cd43ba69e61b8a78edc12f
keexodia <71335482+keexodia@users.noreply.github.com>
2021-09-28 11:06:57 +0900

    404update

--- 404.php
+++ 404.php
@@ -1,3 +1,10 @@
 <?php get_header(); ?>
+<section class="main-content l-single-content p-single">
+        <div class="l-inner p-single__inner">
+            <h2 class="c-title c-title-big p-single__title">
+            お探しの記事は見つかりません。
+            </h2>
 
+</div>
+</section>
 <?php get_footer(); 
